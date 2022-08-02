7ab63c9c1cc7be1d35af22cd872dc1e2b50b8cb9
deepak dhakad <dhakadg1432@gmail.com>
2022-08-02 11:07:20 +0530

    final commit

---
+++ Desktop/weatherapp2/weather_app/config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "8368869125ab2118936eb98b018003a6"
+        // SECRET_KEY: "Your API key which you will get from OPEN WEATHER API"
+    }
+}
+
+module.exports = constants;
